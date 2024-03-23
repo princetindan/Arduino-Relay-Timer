381d823516c687a7ff149c1865edf31937279a31
Prince Tindan <90090677+princetindan@users.noreply.github.com>
2024-03-23 10:15:46 +0000

    Update Kelvins_relay_try_work.ino

--- Kelvins_relay_try_work.ino
+++ Kelvins_relay_try_work.ino
@@ -1,5 +1,6 @@
 int value = 0;
 const byte relay = 6;
+const byte relay2 = 7;
 float voltage;
 float R1 = 47000.0;
 float R2 = 33000.0;
@@ -7,6 +8,7 @@ float R2 = 33000.0;
 void setup() {
   Serial.begin(9600);
   pinMode(relay, OUTPUT);
+  pinMode(relay2, OUTPUT);
 }
 
 void loop() {
@@ -14,14 +16,20 @@ void loop() {
   voltage = value * (5.0 / 1024) * ((R1 + R2) / R2);
 
   if (voltage > 0.01) {
-    digitalWrite(relay, HIGH);
+    digitalWrite(relay, LOW);
+    digitalWrite(relay2,HIGH);
     Serial.print("Voltage = ");
     Serial.println(voltage);
     delay(500);
   } else {
-    digitalWrite(relay, LOW);
+    digitalWrite(relay, HIGH);
+    delay(1800000);
+    Serial.print("Voltage = ");
+    Serial.println(voltage);
+    digitalWrite(relay2,LOW);
     Serial.print("Voltage = ");
     Serial.println(voltage);
     delay(500);
+    
   }
 }
